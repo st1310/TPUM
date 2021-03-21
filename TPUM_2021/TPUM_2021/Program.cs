8113e766c12b7e1f0606f43bb2d7ce738224d7a0
st1310 <stetsenkoandrew@gmail.com>
2021-03-21 21:57:07 +0100

    Initial project and tests added

---
+++ TPUM_2021/TPUM_2021/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TPUM_2021
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string TestMe()
+        {
+            return "TestMe";
+        }
+    }
+}
