11b507fbcc85f9bb488ac301a8d2aaaeb79a1548
Jędrzej Skoczylas <jejakx@gmail.com>
2021-04-18 23:34:16 +0200

    Some features added.

---
+++ TPUM_2021/TPUM_2021.Logic/TickEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPUM_2021.Logic
+{
+    public class TickEventArgs : EventArgs
+    {
+        public TickEventArgs(long counter)
+        {
+            Counter = counter;
+        }
+        public long Counter
+        {
+            get;
+            private set;
+        }
+    }
+}
--- TPUM_2021/TPUM_2021.Presentation/ViewModel/MainViewModel.cs
+++ TPUM_2021/TPUM_2021.Presentation/ViewModel/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Windows.Input;
 using TPUM_2021.Logic;
@@ -10,6 +12,7 @@ namespace TPUM_2021.Presentation.ViewModel
     internal class MainViewModel : BaseViewModel
     {
         public ICommand BuyCommand { get; }
+        public ICommand AddSomeProductsCommand { get; }
 
         private ObservableCollection<ProductDto> _Products;
         private ObservableCollection<ProductDto> _CustomerProducts;
@@ -21,6 +24,11 @@ namespace TPUM_2021.Presentation.ViewModel
 
         private ProductDto _CurrentProduct;
         private CustomerDto _CurrentCustomer;
+
+        private ReactiveTimer _ReactiveTimer;
+        private IObservable<EventPattern<TickEventArgs>> _tickObservable;
+        private IDisposable _observer;
+
 
         public ObservableCollection<ProductDto> Products
         {
@@ -90,6 +98,7 @@ namespace TPUM_2021.Presentation.ViewModel
         public MainViewModel()
         {
             BuyCommand = new RelayCommand(BuyCurrentProduct);
+            AddSomeProductsCommand = new RelayCommand(SetTimer);
 
             _ProductQuery = DependencyResolver.ProductQuery;
             _CustomerQuery = DependencyResolver.CustomerQuery;
@@ -102,6 +111,37 @@ namespace TPUM_2021.Presentation.ViewModel
             _Products = new ObservableCollection<ProductDto>(_ProductQuery.GetAvailableProducts());
             _CustomerProducts = new ObservableCollection<ProductDto>(_ProductQuery.GetProductsByCustomerId(_CurrentCustomer.Id));
             _CurrentProduct = Products[0];
+        }
+
+        public void SetTimer()
+        {
+            SetReactiveTimer(TimeSpan.FromSeconds(5));
+        }
+
+        public void SetReactiveTimer(TimeSpan period)
+        {
+            _ReactiveTimer = new ReactiveTimer(period);
+            _tickObservable = Observable.FromEventPattern<TickEventArgs>(_ReactiveTimer, "Tick");
+            _observer = _tickObservable.Subscribe(x => AddPrice());
+
+            _ReactiveTimer.Start();
+        }
+
+        public void AddPrice()
+        {
+            ObservableCollection<ProductDto> productsTemp = Products;
+
+            foreach (ProductDto product in productsTemp)
+            {
+                product.Price += 5;
+
+                if (product.Price >= 50)
+                {
+                    product.Price = 52;
+                }
+            }
+
+            Products = new ObservableCollection<ProductDto>(productsTemp);
         }
 
         public void BuyCurrentProduct()
