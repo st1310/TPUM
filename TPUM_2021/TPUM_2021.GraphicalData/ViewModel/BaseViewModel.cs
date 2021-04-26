e705ec525af61859d9768dd2c9e7a47178dc6450
Jędrzej Skoczylas <jejakx@gmail.com>
2021-04-26 23:39:35 +0200

    new presentation layer

---
+++ TPUM_2021/TPUM_2021.GraphicalData/ViewModel/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TPUM_2021.GraphicalData.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
