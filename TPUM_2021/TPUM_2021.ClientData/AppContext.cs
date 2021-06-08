e5f2559b19291b49b19338a3f9b82f2111b34715
AS <stetsenkoandrew@gmail.com>
2021-06-08 21:44:10 +0200

    Shared Model used for communication

--- TPUM_2021/TPUM_2021.ClientData/AppContext.cs
+++ TPUM_2021/TPUM_2021.ClientData/AppContext.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.Json;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 using TPUM_2021.CommonData;
 using TPUM_2021.CommonData.Model;
 using TPUM_2021.CommonWebSocketApi;
+using TPUM_2021.OPCUAdependencies.Model;
 
 [assembly: InternalsVisibleTo("TPUM_2021.Test")]
 
@@ -61,8 +63,29 @@ namespace TPUM_2021.ClientData
         public string Serialize(Type type, object obj)
         {
             using MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer;
+            Type dataType = typePairs[type];
 
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typePairs[type]);
+            if (dataType == typeof(Customer))
+            {
+                obj = Map<Customer>(obj);
+                serializer = new DataContractJsonSerializer(typeof(List<CustomerS>)); // TOut
+            }
+            else if (dataType == typeof(Product))
+            {
+                obj = Map<Product>(obj);
+                serializer = new DataContractJsonSerializer(typeof(List<ProductS>)); // TOut
+            }
+            else if (dataType == typeof(Invoice))
+            {
+                obj = Map<Invoice>(obj);
+                serializer = new DataContractJsonSerializer(typeof(List<InvoiceS>)); // TOut
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
             serializer.WriteObject(stream, obj);
 
             byte[] json = stream.ToArray();
@@ -85,24 +108,29 @@ namespace TPUM_2021.ClientData
             Type dataType = typePairs[typeof(TOut)];
             DataContractJsonSerializer serializer;
 
+            IEnumerable<TOut> result;
+
             if (dataType == typeof(Customer))
             {
-                serializer = new DataContractJsonSerializer(typeof(List<Customer>)); // TOut
+                serializer = new DataContractJsonSerializer(typeof(List<CustomerS>)); // TOut
+                result = new List<CustomerS>(serializer.ReadObject(stream) as IEnumerable<CustomerS>).Select(x => Map<CustomerS>(x) as ICustomer) as IEnumerable<TOut>;
             }
             else if (dataType == typeof(Product))
             {
-                serializer = new DataContractJsonSerializer(typeof(List<Product>)); // TOut
+                serializer = new DataContractJsonSerializer(typeof(List<ProductS>)); // TOut
+                result = new List<ProductS>(serializer.ReadObject(stream) as IEnumerable<ProductS>).Select(x => Map<ProductS>(x) as IProduct) as IEnumerable<TOut>;
             }
             else if (dataType == typeof(Invoice))
             {
-                serializer = new DataContractJsonSerializer(typeof(List<Invoice>)); // TOut
+                serializer = new DataContractJsonSerializer(typeof(List<InvoiceS>)); // TOut
+                result = new List<InvoiceS>(serializer.ReadObject(stream) as IEnumerable<InvoiceS>).Select(x => Map<InvoiceS>(x) as IInvoice) as IEnumerable<TOut>;
             }
             else
             {
                 throw new ArgumentException();
             }
 
-            return serializer.ReadObject(stream) as IEnumerable<TOut>;
+            return result;
         }
 
         public string GetTypeName(Type type)
@@ -123,6 +151,57 @@ namespace TPUM_2021.ClientData
             {
                 throw new ArgumentException();
             }
+        }
+
+        private object Map<TIn>(object obj)
+        {
+            if (typeof(TIn) == typeof(Customer))
+            {
+                Customer tmp = obj as Customer;
+                return new CustomerS
+                {
+                    Id = tmp.Id,
+                    Name = tmp.Name,
+                    Funds = tmp.Funds
+                };
+            }
+
+            if (typeof(TIn) == typeof(Product))
+            {
+                Product tmp = obj as Product;
+                return new ProductS
+                {
+                    Id = tmp.Id,
+                    Name = tmp.Name,
+                    CustomerId = tmp.CustomerId,
+                    Price = tmp.Price
+                };
+            }
+
+            if (typeof(TIn) == typeof(CustomerS))
+            {
+                CustomerS tmp = obj as CustomerS;
+                return new Customer
+                {
+                    Id = tmp.Id,
+                    Name = tmp.Name,
+                    Funds = tmp.Funds
+                };
+            }
+
+            if (typeof(TIn) == typeof(ProductS))
+            {
+                ProductS tmp = obj as ProductS;
+                return new Product
+                {
+                    Id = tmp.Id,
+                    Name = tmp.Name,
+                    CustomerId = tmp.CustomerId,
+                    Price = tmp.Price
+                };
+            }
+
+            throw new ArgumentOutOfRangeException();
         }
 
         public IDisposable Subscribe(IObserver<string> observer)
